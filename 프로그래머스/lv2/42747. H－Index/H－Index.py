981367385a303ace8a4cc2b9e6cef9257908e7b8
kojunwoan <67447278+kojunwoan@users.noreply.github.com>
2023-07-10 16:33:28 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,11 +1,3 @@
 def solution(citations):
     citations.sort(reverse=True)
-    h, t = 0, len(citations)
-    for c in citations:
-        while t > c:
-            t -= 1
-            if h == t:
-                return h
-        h += 1
-        if h == t:
-            return h
+    return max(map(min, enumerate(citations, start=1)))
