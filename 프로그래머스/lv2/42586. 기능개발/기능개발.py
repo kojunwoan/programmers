e67390ba466dad4b5cd0ba7b6cd76e8f861c7dfd
kojunwoan <67447278+kojunwoan@users.noreply.github.com>
2023-07-14 15:02:04 +0900

    [level 2] Title: 기능개발, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,9 +1,7 @@
-from math import ceil
-from collections import deque
 def solution(progresses, speeds):
     last, res = 0, []
     for p, s in zip(progresses, speeds):
-        date = ceil((100 - p) / s)
+        date = -((p - 100) // s)
         if date > last:
             last = date
             res.append(1)
