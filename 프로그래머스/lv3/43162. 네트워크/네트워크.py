0951ad7e187c7c391b9b4da5d7d2c3b27565ecb1
kojunwoan <67447278+kojunwoan@users.noreply.github.com>
2023-07-21 15:10:32 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,18 +1,36 @@
-def solution(n, computers):
+def solution1(n, computers): #BFS
     group = 0
     queue = []
     visited = set()
     
-    for i, node in enumerate(computers):
+    for i, computer in enumerate(computers):
         if i not in visited:
-            queue.append(node)
+            queue.append(computer)
             visited.add(i)
             group += 1
             
         while queue:
-            for i, node in enumerate(queue.pop(0)):
-                if node and i not in visited:
+            for i, v in enumerate(queue.pop(0)):
+                if v and i not in visited:
                     queue.append(computers[i])
                     visited.add(i)
+    
+    return group
+
+def solution(n, computers): #DFS
+    group = 0
+    visited = set()
+    
+    def dfs(computer):
+        for j, v in enumerate(computer):
+            if v and j not in visited:
+                visited.add(j)
+                dfs(computers[j])
+    
+    for i, computer in enumerate(computers):
+        if i not in visited:
+            visited.add(i)
+            group += 1
+            dfs(computer)
     
     return group
