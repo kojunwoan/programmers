fd6c6d9811bcc7e50bdee4be8900b19e593483a3
kojunwoan <67447278+kojunwoan@users.noreply.github.com>
2023-07-21 18:09:05 +0900

    [level 3] Title: 단어 변환, Time: 0.66 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -3,7 +3,7 @@ def solution(begin, target, words):
     queue = list()
     visited = set()
     
-    def find(n, begin):
+    def bfs(n, begin):
         for i in range(len(begin)):
             t = ""
             for j in range(len(begin)):
@@ -17,11 +17,11 @@ def solution(begin, target, words):
                     queue.append((n + 1, w))
                     visited.add(w)
     
-    find(0, begin)
+    bfs(0, begin)
     while queue:
         n, begin = queue.pop(0)
         if begin == target:
             return n
-        find(n, begin)
+        bfs(n, begin)
     
     return 0
