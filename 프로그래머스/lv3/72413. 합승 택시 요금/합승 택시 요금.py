47332b340e35fe939f664f318182b9ba4fa8922a
kojunwoan <67447278+kojunwoan@users.noreply.github.com>
2023-07-26 11:25:24 +0900

    [level 3] Title: 합승 택시 요금, Time: 152.45 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,5 +1,5 @@
 from heapq import heappush, heappop
-def solution(n, s, a, b, fares):
+def solution1(n, s, a, b, fares):
     path = {}
     for n1, n2, c in fares:
         path.setdefault(n1, {n1: 0})[n2] = c
@@ -19,3 +19,22 @@ def solution(n, s, a, b, fares):
         costs.append(cost)
     
     return min(sum(map(lambda d: d[i], costs)) for i in costs[0])
+
+def solution(n, s, a, b, fares):
+    d = [ [ 20000001 for _ in range(n) ] for _ in range(n) ]
+    for x in range(n):
+        d[x][x] = 0
+    for x, y, c in fares:
+        d[x-1][y-1] = c
+        d[y-1][x-1] = c
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if d[j][k] > d[j][i] + d[i][k]:
+                    d[j][k] = d[j][i] + d[i][k]
+
+    minv = 40000002
+    for i in range(n):
+        minv = min(minv, d[s-1][i]+d[i][a-1]+d[i][b-1])
+    return minv
