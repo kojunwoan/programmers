0f2037e3ec0297520afdef85cc7fe17f2b041829
kojunwoan <67447278+kojunwoan@users.noreply.github.com>
2023-09-11 07:19:40 +0900

    [level 2] Title: 모음 사전, Time: 0.82 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,4 +1,9 @@
+from itertools import product
 def solution(word):
+    return sorted("".join(p) for n in range(5) for p in product('AEIOU', repeat=n+1)).index(word) + 1
+
+
+def solution1(word):
     global cnt
     cnt = 0
     
